cad58ee4c41bd984161b97a7c832aa6921da9137
0417taehyun <0417taehyun@gmail.com>
2022-08-23 16:11:05 +0900

    :recycle: [2022년도 여름방학 언어공학 워크숍: 자료 구조와 알고리즘] 1일차: 자료 구조와 알고리즘 소개, 선형 자료 구조(스택, 큐)

--- 2022/02_여름방학/언어공학 워크숍/1일차/stack.py
+++ 2022/02_여름방학/언어공학 워크숍/1일차/stack.py
@@ -1,6 +1,13 @@
 # Python의 클래스와 리스트를 활용한 스택(Stack) 구현
 
 class Stack:
+    """
+    아래와 같이 멤버 변수(Member Variable) -인스턴스 변수(Instacne Variable)라고도 함- 가 아닌
+    클래스 변수(Class Variable)로 stack_size 변수를 정의해줄 수도 있다.
+    이 경우 생성된 모든 스택 객체가 동일한 스택 사이즈를 공유한다는 걸 가정한다.
+    
+    stack_size: int = 3
+    """
     def __init__(self, size: int) -> None:
         self.limit_size: int = size
         self.current_size: int = 0
@@ -44,4 +51,3 @@ if __name__ == "__main__":
 
     stack()
     stack.pop()
-    
